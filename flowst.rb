1606e450fcfa13d6897118e46071d4768c42eb1e
ben-toker <117331544+ben-toker@users.noreply.github.com>
2023-09-17 13:12:15 -0400

    first release

---
+++ flowst.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Flowst < Formula
+  desc "Pomodoro Study Tool written in Rust"
+  homepage "https://github.com/ben-toker/flowst"
+  url "https://github.com/ben-toker/flowst/releases/download/1.1/flowst-mac.tar.gz
+  sha256 "4c6fb85a6adb5be9a86268106e8d65143e1f784beda840f8adfff226e79982e4"
+  version "0.1.0"
+
+  def install
+    bin.install "flowst"
+  end
+end
